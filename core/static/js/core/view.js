c1398cdea5bddeea45edb69718e89abb976f2502
Wurka <wurka_13@mail.ru>
2019-02-15 08:27:32 +0300

    vuejs-зация

--- core/static/js/core/view.js
+++ core/static/js/core/view.js
@@ -1,6 +1,32 @@
 var app = new Vue({ 
     el: '#app',
+    delimiters: ['[[', ']]'],
     data: {
-        message: 'Hello Vue!'
+        showNewTarget: false,
+        authors: {
+            10: { name: "Саша"},
+            20: { name: "Маша"},
+            33: { name: "Саша и Маша"}
+        },
+        targets: {
+            10: { title: "Проститутки" },
+            12: { title: "Наркотики" },
+            13: { title: "Оружие" },
+            14: { title: "Проститутки" },
+            15: { title: "Исподведь" }
+        }
+    },
+    methods: {
+        authorOnChange: function (e) {
+            console.log("authorOnChange");
+            console.log(e.target.value);
+        },
+        targetOnChange: function (e) {
+            console.log("targetOnChange");
+            console.log(e.target.value);
+        }
+    },
+    mounted: function () {
+        $(".new-target").show();
     }
 });
