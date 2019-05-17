d942de9c23323ea4e20dae8e16210a2ce48cb508
Wurka-lenovo <wurka_13@mail.ru>
2019-05-17 08:55:18 +0300

    url fix

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 import core.views
 
 urlpatterns = [
-	path('', core.views.main),
+	#path('', core.views.main),
+	path('', core.views.view),
 	path('test/', core.views.test),
 	path('x/', core.views.main),
 	path("new-target/", core.views.new_target),
--- laksa/urls.py
+++ laksa/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	path('laksa/',  include('core.urls'))
+	path('',  include('core.urls'))
 ]
